14e973c8734dc4ba34e717293dea06a2aeea02eb
ericqd <eric_dao@hotmail.com>
2020-03-02 15:12:48 -0800

    updated

--- MemoryGame.java
+++ MemoryGame.java
@@ -9,13 +9,19 @@ public class MemoryGame{
         String fileChoice = getFileChoice();
         readFile(fileChoice,cardValue);
         shuffleDeck(cardValue);
-        displayBoard(cardValue, cardState);
-        String userSelection1 = getChoice(cardValue);
-        String userSelection2= getChoice(cardValue);
-        System.out.println(userSelection1);
-        System.out.println(userSelection2);
-
+        int userSelection1 = getChoice();
+        flipChoice(userSelection1,cardValue,cardState, true);
+        int userSelection2= getChoice();
+        flipChoice(userSelection2,cardValue,cardState,true);
+        boolean match = isMatch(userSelection1,userSelection2,cardValue);
+        int numOfDown= 16;
+            while(numOfDown > 0){
+                if(match == true){
+                    System.out.println("Cards match!");
+                    numOfDown -=2;
+                }
     }
+}
 
 
     public static String getFileChoice() {
@@ -87,77 +93,208 @@ public class MemoryGame{
     }
     public static void displayBoard(String[][] array, boolean[][] array2){
         int num = 0;
-        while(num != 16){
             for(int i = 0; i < 4; i++){
-                System.out.println("+----+   +----+   +----+   +----+");
+                System.out.println("+- - - -+" + " " + "+- - - -+" + " " + "+- - - -+" + " " + "+- - - -+");
+                System.out.println("|       |" + " " + "|       |" + " " + "|       |" + " " + "|       |");
                 for(int j = 0; j < 4; j++){
+                    String size = Integer.toString(num);
                     if(array2[i][j]){
                         System.out.print(array[i][j]);
                         System.out.println(" ");
                     }
+                    else if(size.length() >=2){
+                        num++;
+                        System.out.print("|   " +(num)+ "  |" + " ");
+                    }
                     else{
                         num++;
-                        System.out.print("|   " +(num)+ " | ");
+                        System.out.print("|   " +(num)+ "   |" + " ");
                     }
                 }
-                System.out.println();
+                System.out.println("");
+                System.out.println("|       |" + " " + "|       |" + " " + "|       |" + " " + "|       |");
+                System.out.println("|       |" + " " + "|       |" + " " + "|       |" + " " + "|       |");
+                System.out.println("+- - - -+" + " " + "+- - - -+" + " " + "+- - - -+" + " " + "+- - - -+");
+
             }
-            System.out.println("+----+   +----+   +----+   +----+");
+            System.out.println("");
         }
-    }
-    public static String getChoice(String[][] array){
+    public static int getChoice(){
         System.out.println("Enter choice: ");
         int userChoice = CheckInput.getIntRange(1,16);
+        // String location = array[row][col];
+        return userChoice;
+    }
+    public static Boolean flipChoice(int input,String[][] array, boolean[][] array2, boolean flip){
         int row = 0;
         int col = 0;
-        if(userChoice <= 4 && userChoice % 4 != 0){
-            row = 0; 
-            col = (userChoice % 4) - 1; 
-        }
-        else if(userChoice > 4 && userChoice < 8 && userChoice % 4 != 0){
-            row = 1;
-            col = (userChoice % 4) - 1;
-        }
-        else if (userChoice > 8 && userChoice < 12 && userChoice % 4 != 0){
-            row = 2;
-            col = (userChoice % 4)-1; 
-        }
-        else if(userChoice > 12 && userChoice < 16 && userChoice % 4 != 0){
-            row = 3;
-            col = (userChoice % 4)-1; 
-        }
-        else if(userChoice == 4){
-            row = 0;
-            col = 3;
-        }
-        else if(userChoice == 8){
-            row = 1;
-            col = 3;
-        }
-        else if(userChoice == 12){
-            row = 2;
-            col = 3;
-        }
-        else if(userChoice == 16){
-            row = 3;
-            col = 3;
-        }
-        String location = array[row][col];
-        return location;
-    }
-    public static void flipChoice(String selection1, String selection2, boolean[][] array2, String[][] array, boolean flip){
+        displayBoard(array,array2);
         for(int i = 0; i < array2.length; i++){
             for(int j = 0; j < array2[i].length ;j++ ){
                 array2[i][j] = false;
             }
         }
-        
+        if(input <= 4 && input % 4 != 0){
+            row = 0; 
+            col = (input % 4) - 1; 
+        }
+        else if(input > 4 && input < 8 && input % 4 != 0){
+            row = 1;
+            col = (input % 4) - 1;
+        }
+        else if (input > 8 && input < 12 && input % 4 != 0){
+            row = 2;
+            col = (input % 4)-1; 
+        }
+        else if(input > 12 && input < 16 && input % 4 != 0){
+            row = 3;
+            col = (input % 4)-1; 
+        }
+        else if(input == 4){
+            row = 0;
+            col = 3;
+        }
+        else if(input == 8){
+            row = 1;
+            col = 3;
+        }
+        else if(input == 12){
+            row = 2;
+            col = 3;
+        }
+        else if(input == 16){
+            row = 3;
+            col = 3;
+        }
+        boolean location = array2[row][col];
+        if(location == false){
+            location = flip;
+        }
+        if(checkFlipped(input,array2)){
+            System.out.println("Card is already flipped");
+            flipChoice(input,array,array2,flip);
+        }
+        return location;
     }
-    public static boolean isMatch(String selection1, String selection2, String[][]array){
+    public static Boolean isMatch(int input1, int input2, String[][]array){
+        int row = 0;
+        int col = 0;
+        int row2 = 0;
+        int col2 = 0;
+        if(input1 <= 4 && input1 % 4 != 0){
+            row = 0; 
+            col = (input1 % 4) - 1; 
+        }
+        else if(input1 > 4 && input1 < 8 && input1 % 4 != 0){
+            row = 1;
+            col = (input1 % 4) - 1;
+        }
+        else if (input1 > 8 && input1 < 12 && input1 % 4 != 0){
+            row = 2;
+            col = (input1 % 4)-1; 
+        }
+        else if(input1 > 12 && input1 < 16 && input1 % 4 != 0){
+            row = 3;
+            col = (input1 % 4)-1; 
+        }
+        else if(input1 == 4){
+            row = 0;
+            col = 3;
+        }
+        else if(input1 == 8){
+            row = 1;
+            col = 3;
+        }
+        else if(input1 == 12){
+            row = 2;
+            col = 3;
+        }
+        else if(input1 == 16){
+            row = 3;
+            col = 3;
+        }
+        String location1 = array[row][col];
+        if(input2 <= 4 && input2 % 4 != 0){
+            row2 = 0; 
+            col2 = (input2 % 4) - 1; 
+        }
+        else if(input2 > 4 && input2 < 8 && input2 % 4 != 0){
+            row2 = 1;
+            col2 = (input2 % 4) - 1;
+        }
+        else if (input2 > 8 && input2 < 12 && input2 % 4 != 0){
+            row2 = 2;
+            col2 = (input2 % 4)-1; 
+        }
+        else if(input2 > 12 && input2 < 16 && input2 % 4 != 0){
+            row2 = 3;
+            col2 = (input2 % 4)-1; 
+        }
+        else if(input2 == 4){
+            row2 = 0;
+            col2 = 3;
+        }
+        else if(input2 == 8){
+            row2 = 1;
+            col2 = 3;
+        }
+        else if(input2 == 12){
+            row2 = 2;
+            col2 = 3;
+        }
+        else if(input2 == 16){
+            row2 = 3;
+            col2 = 3;
+        }
+        String location2 = array[row2][col2];
         boolean match = false;
-        if(selection1 == selection2){
+        if(location1 == location2){
             match = true;
+        }
+        else{
+            System.out.println("Don't Match");
         }
         return match;
     }
+    public static Boolean checkFlipped(int input,boolean[][] array2){
+        int row = 0;
+        int col = 0;
+        if(input <= 4 && input % 4 != 0){
+            row = 0; 
+            col = (input % 4) - 1; 
+        }
+        else if(input > 4 && input < 8 && input % 4 != 0){
+            row = 1;
+            col = (input % 4) - 1;
+        }
+        else if (input > 8 && input < 12 && input % 4 != 0){
+            row = 2;
+            col = (input % 4)-1; 
+        }
+        else if(input > 12 && input < 16 && input % 4 != 0){
+            row = 3;
+            col = (input % 4)-1; 
+        }
+        else if(input == 4){
+            row = 0;
+            col = 3;
+        }
+        else if(input == 8){
+            row = 1;
+            col = 3;
+        }
+        else if(input == 12){
+            row = 2;
+            col = 3;
+        }
+        else if(input == 16){
+            row = 3;
+            col = 3;
+        }
+        boolean location = array2[row][col];
+        if(location){
+            return true;
+        }
+        return false;
+        }
 }
