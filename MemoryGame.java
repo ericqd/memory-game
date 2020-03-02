e702424e7fdb4d0cfbe67a185fe30e6dc59fa625
ericqd <eric_dao@hotmail.com>
2020-03-01 18:31:40 -0800

    yeet

--- MemoryGame.java
+++ MemoryGame.java
@@ -8,6 +8,10 @@ public class MemoryGame{
         boolean[][] cardState = new boolean[4][4];
         String fileChoice = getFileChoice();
         readFile(fileChoice,cardValue);
+        shuffleDeck(cardValue);
+        displayBoard(cardValue, cardState);
+        int userChoice = getChoice();
+
     }
 
 
@@ -43,11 +47,32 @@ public class MemoryGame{
                 for(int j = 0; j < col && read.hasNextLine(); j++){
                     String card = read.nextLine();
                     array[i][j] = card;
+                    j++;
+                    array[i][j] = card;
                 }
             } 
             read.close();
         } catch(FileNotFoundException fnf){
             System.out.println("File was not found");
+        }
+        return array;
+    }
+    public static String[][] shuffleDeck(String [][] array){
+        int count = 0;
+        int row = 4;
+        int col = 4;
+        Random rand = new Random();
+        while(count != 100){
+            count++;
+            for(int i = array.length - 1; i > 0; i-- ){
+                for(int j = array[i].length -1 ; j > 0; j--){
+                    int m = rand.nextInt(i+1);
+                    int n = rand.nextInt(j+1);
+                    String temp = array[i][j];
+                    array[i][j] = array[m][n];
+                    array[m][n] = temp;
+                }
+            }
         }
         for(int i=0;i < row; i++){
             for(int j=0; j < col; j++){
@@ -56,5 +81,28 @@ public class MemoryGame{
             System.out.println();
         }
         return array;
+    }
+    public static void displayBoard(String[][] array, boolean[][] array2){
+        for(int i = 0; i < 4; i++){
+            System.out.println("+-------+   +-------+   +-------+   +-------+");
+            for(int j = 0; j < 4; j++){
+                if(array2[i][j]){
+                    System.out.print(array[i][j]);
+                    System.out.println(" ");
+                }
+                else{
+                    System.out.print("|" + (i+1) + "      |   ");
+                }
+            }
+            System.out.println();
+        }
+        System.out.println("+-------+   +-------+   +-------+   +-------+");
+    }
+    public static int getChoice(){
+        int userChoice = CheckInput.getIntRange(1,16);
+        return userChoice;
+    }
+    public static void flipChoice(int input, boolean[][] array2){
+
     }
 }
